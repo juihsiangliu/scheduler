41870d7b19fb11c6b1ec1a3330a4c15bb2fb991f
juihsiangliu <juihsiangliu@gmail.com>
2015-07-08 23:28:15 +0800

    refactor

--- src/parser.cpp
+++ src/parser.cpp
@@ -14,33 +14,33 @@ bool parser(const char* filename)
 
 	char buf[bufSize];
 	enum stage_e {init, resource, task};
-    enum stage_e stage = init;
-	
-	while(1) {
-		memset(buf, 0, bufSize * sizeof(char));
-		char* ch = fgets(buf, bufSize, fp);
-		if (ch == NULL) {
-			break;
-		}
-		buf[strlen(buf)-1] = 0;
+	enum stage_e stage = init;
 
-		if (strcmp(buf, "#resource_list") == 0) {
-			stage = resource;
-		}
-		else if (strcmp(buf, "#task_list") == 0) {
-			stage = task;
-		}
+    while(1) {
+        memset(buf, 0, bufSize * sizeof(char));
+        char* ch = fgets(buf, bufSize, fp);
+        if (ch == NULL) {
+            break;
+        }
+        buf[strlen(buf)-1] = 0;
 
-		if (stage == resource) {
-			parseResourcePart(buf);
-		}
-		else if (stage == task) {
-			parseTaskPart(buf);
-		}
-		else {
-			printf("init %s\n", buf);
-		}
-	}
+        if (strcmp(buf, "#resource_list") == 0) {
+            stage = resource;
+        }
+        else if (strcmp(buf, "#task_list") == 0) {
+            stage = task;
+        }
+
+        if (stage == resource) {
+            parseResourcePart(buf);
+        }
+        else if (stage == task) {
+            parseTaskPart(buf);
+        }
+        else {
+            printf("init %s\n", buf);
+        }
+    }
 
 	fclose(fp);
 	return true;
